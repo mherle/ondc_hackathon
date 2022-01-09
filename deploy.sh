0dd11f31d43edf34a684615281c0a1a300500180
Mahesh Herle <mahesh@goodbox.in>
2022-01-09 16:22:50 +0530

    aded ssh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh ubuntu@api/gdbx.in cd /opt/ondc-hackathon/ && git pull && sudo service ondc-hackathon restart && sudo service ondc-hackathon status
+ssh ubuntu@api.gdbx.in cd /opt/ondc-hackathon/ && git pull && sudo service ondc-hackathon restart && sudo service ondc-hackathon status
