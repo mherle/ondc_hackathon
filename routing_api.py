2d8103c05258446234218f9516788a8f90e99d7f
sunil <sunil.k@goodbox.in>
2022-01-09 23:36:51 +0530

    fixing case d link

--- routing_api.py
+++ routing_api.py
@@ -32,10 +32,6 @@ def post_request_logging(response):
     return response
 
 
-@routingapi_blueprint.route("/casec", methods=["GET"])
-def index():
-    return render_template("challenge7/casec.html")
-
 @routingapi_blueprint.route("/casea", methods=["GET"])
 def casea():
     return render_template("challenge7/casea.html")
@@ -43,6 +39,14 @@ def casea():
 @routingapi_blueprint.route("/caseb", methods=["GET"])
 def caseb():
     return render_template("challenge7/caseb.html")
+
+@routingapi_blueprint.route("/casec", methods=["GET"])
+def casec():
+    return render_template("challenge7/casec.html")
+
+@routingapi_blueprint.route("/cased", methods=["GET"])
+def cased():
+    return render_template("challenge7/casea.html")
 
 @routingapi_blueprint.route("/api/solve", methods=["POST", "OPTIONS"])
 def solve():
