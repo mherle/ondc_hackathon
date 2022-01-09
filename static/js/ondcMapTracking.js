bd91e5aaad6f3b7645f2a529db5525a0dd60b122
sunil <sunil.k@goodbox.in>
2022-01-09 17:31:02 +0530

    modified render view page

--- static/js/ondcMapTracking.js
+++ static/js/ondcMapTracking.js
@@ -15,64 +15,53 @@ function drawRoute(routingPath, selectedIndex){
     var osm = window.L.tileLayer ('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo (map_init);
+
+    var startIcon = L.icon({
+        // iconUrl: 'racing-flag.png',
+        iconUrl: "http://leafletjs.com/examples/custom-icons/leaf-green.png",
+        iconSize: [45, 70],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76],
+        // shadowUrl: 'racing-flag.png',
+        shadowUrl: "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+        shadowSize: [68, 95],
+        shadowAnchor: [22, 94]
+    });
+
+    var endIcon = L.icon({
+        iconUrl: "http://leafletjs.com/examples/custom-icons/leaf-red.png",
+        iconSize: [45, 70],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76],
+        // shadowUrl: 'finish.png',
+        shadowUrl: "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+        shadowSize: [68, 95],
+        shadowAnchor: [22, 94]
+    });
     
     if(selectedIndex !== -1){
-        selected_route_path_index = selectedIndex
-        selected_route_waypoints = routingPath[selected_route_path_index].map((item, index) => {
-            console.log('item is ', item)
-            return [item[0], item[1]]
-        })
-    
-        var startIcon = L.icon({
-            // iconUrl: 'racing-flag.png',
-            iconUrl: "http://leafletjs.com/examples/custom-icons/leaf-green.png",
-            iconSize: [45, 70],
-            iconAnchor: [22, 94],
-            popupAnchor: [-3, -76],
-            // shadowUrl: 'racing-flag.png',
-            shadowUrl: "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
-            shadowSize: [68, 95],
-            shadowAnchor: [22, 94]
-        });
-    
-        var endIcon = L.icon({
-            iconUrl: "http://leafletjs.com/examples/custom-icons/leaf-red.png",
-            iconSize: [45, 70],
-            iconAnchor: [22, 94],
-            popupAnchor: [-3, -76],
-            // shadowUrl: 'finish.png',
-            shadowUrl: "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
-            shadowSize: [68, 95],
-            shadowAnchor: [22, 94]
-        });
-            
-        // var polyline = window.L.polyline(selected_route_waypoints, {color: '#3388ff', weight: 3}).addTo(map_init);
-        var polyline = window.L.polyline(selected_route_waypoints, {weight: 5});
-        // PLOTTING THE POLYLINES
-        setPolylineColors(polyline,['#00FF00','#808080'])
-        function setPolylineColors(line,colors){
-          selected_route_waypoints.forEach((lat_lng, index) => {
-              console.log('lat lng is ', lat_lng)
-              var current_path_lat_lngs = {"lat": lat_lng[0], "lng": lat_lng[1]}
-              var next_selected_route_index = index === selected_route_waypoints.length - 1 ? index : index + 1
-              var next_path_lat_lngs = {"lat": selected_route_waypoints[next_selected_route_index][0], "lng": selected_route_waypoints[next_selected_route_index][1]}
-            var poly =  L.polyline([current_path_lat_lngs,next_path_lat_lngs],{color: colors[index%2 === 0 ? 0 : 1], weight: 5}).addTo(map_init);
-          })
-    
+        for(let i=0; i < routingPath.length; i++){
+            selected_route_waypoints = routingPath[i].map((item, index) => {
+                console.log('item is ', item)
+                return [item[0], item[1]]
+            })
+            var polyline = window.L.polyline(selected_route_waypoints, {color: generateRandomColor(), weight: 3}).addTo(map_init);
+            plotMarkers(selected_route_waypoints, i)
         }
-    
-        plotMarkers()
-        
-        function plotMarkers(){
-            selected_route_waypoints.forEach((item, index) => {
+     
+        function plotMarkers(selectedRoute, currentRouteIndex){
+            selectedRoute.forEach((item, index) => {
                 let markerTitle = ""
+                let markerContent = ""
                 if (index === 0){
                     markerTitle = "Start"
-                } else if( index === selected_route_waypoints.length - 1){
+                    markerContent = "Route "+ currentRouteIndex + " start"
+                } else if( index === selectedRoute.length - 1){
                     markerTitle = "End"
+                    markerContent = "Route "+ currentRouteIndex + " end"
                 }
                 if (markerTitle){
-                    (window.L.marker([item[0], item[1]], {icon: markerTitle === "Start" ? startIcon : endIcon}).addTo(map_init)).bindPopup("<b>" + markerTitle+ "</b><br></br>" + markerTitle + " path").openPopup();
+                    (window.L.marker([item[0], item[1]], {icon: markerTitle === "Start" ? startIcon : endIcon}).addTo(map_init)).bindPopup("<b>" + markerTitle+ "</b><br></br>" + markerContent).openPopup();
                 } else {
                     window.L.marker([item[0], item[1]]).addTo(map_init)
                 }
@@ -81,4 +70,17 @@ function drawRoute(routingPath, selectedIndex){
     
     }
     
+}
+
+
+function generateRandomColor(){
+    return '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+}
+
+function clearMapContents(){
+    var map_init = window.map_init
+    map_init.eachLayer(function (layer) {
+        map_init.removeLayer(layer);
+    });
+    drawRoute([], -1)
 }
